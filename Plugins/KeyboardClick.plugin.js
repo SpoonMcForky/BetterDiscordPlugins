99f7d52e46251af8c705ddd0add6c102c09fdb3e
SpoonMcForky <55011606+SpoonMcForky@users.noreply.github.com>
2021-08-31 13:58:58 -0400

    fix updater branch

--- Plugins/KeyboardClick.plugin.js
+++ Plugins/KeyboardClick.plugin.js
@@ -26,7 +26,7 @@ module.exports = (() => {
             version: '2.0.8',
             description: 'Plays a click sound when a button is pressed, similarily to OperaGX (uses the same sounds)',
             github: 'https://github.com/SpoonMcForky/BetterDiscordPlugins',
-            github_raw: 'https://raw.githubusercontent.com/SpoonMcForky/BetterDiscordPlugins/Testing/Plugins/KeyboardClick.plugin.js'
+            github_raw: 'https://raw.githubusercontent.com/SpoonMcForky/BetterDiscordPlugins/main/Plugins/KeyboardClick.plugin.js'
         },
         version: '2.0.8',
         changelogItems: [
